fae929cc5287dc6dfd9ed8378b6d0f3f125762e8
Bryan Koo <baiyankoo@gmail.com>
2026-04-19 10:28:19 +0800

    fix url conflicts

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('dashboard.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.home_redirect, name='home'),
-    path('admin/', views.admin_home, name='admin_home'),
+    path('admin-home/', views.admin_home, name='admin_home'),
     path('student/', views.student_home, name='student_home'),
     path('post/create/', views.create_post, name='create_post'),
     path('post/delete/<int:pk>/', views.delete_post, name='delete_post'),
